7bc5d337f7f8d01224b1e0487bc9fe7c37de712d
Samvdg <1032961@idcollege.nl>
2019-02-05 19:11:06 +0100

    all getters and setters complete

--- Robbe/bereken_btw/rekenBTW_OOP.php
+++ Robbe/bereken_btw/rekenBTW_OOP.php
@@ -1,80 +1,102 @@
 <?php
-include './rekenBTW_OOP';
+require_once './rekenBTW_OOP.php';
 
 class form {
 
-    public $bedrag = 100;                      // het ingevoerde bedrag  (standaard 100)
-    public $btw = 21;                          // percentage btw         (standaard 21)
-    public $inc = true;                        // boolean, om aan te geven of het bedrag inclusief of exclusief btw is
-    public $btwBedrag = $this->calcBtw($inc)   // hoeveelheid btw van het bedrag
-    public $noBtw = $bedrag - $btwBedrag;      // bedrag zonder btw
-    public $incBtw = $bedrag;                  // bedrag met btw
+    public $bedrag = 100;                       // het ingevoerde bedrag  (standaard 100)
+    public $btw = 21;                           // percentage btw         (standaard 21)
+    public $inc = true;                         // boolean, om aan te geven of het bedrag inclusief of exclusief btw is
+    public $btwBedrag = 21;                     // hoeveelheid btw van het bedrag
+    public $noBtw = 79;                         // bedrag zonder btw
+    public $incBtw = 100;                       // bedrag met btw
 
     public function getBedrag(){
-        return $bedrag;
+        return $this->bedrag;
     }
 
     public function setBedrag($bedrag){
-        
+        $this->bedrag = $bedrag;
     }
 
     public function getBtw(){
-        return $btw;
+        return $this->btw;
+    }
+
+    public function setBtw($btw){
+        $this->btw = $btw;
     }
 
     public function getInc(){
-        return $inc;
+        return $this->inc;
+    }
+
+    public function setInc($inc){
+        $this->inc = $inc;
     }
 
     public function getBtwBedrag(){
-        return $btwBedrag;
+        return $this->btwBedrag;
     }
 
-    public function getBedrag(){
-        return $noBtw;
+    public function setBtwBedrag($btwBedrag){
+        $this->btwBedrag = $btwBedrag;
     }
 
-    public function incBtw(){
+    public function getNoBtw(){
+        return $this->noBtw;
+    }
+
+    public function setNoBtw($noBtw){
+        $this->noBtw = $noBtw;
+    }
+
+    public function getIncBtw(){
         return $incBtw;
+    }
+
+    public function setIncBtw($incBtw){
+        $this->incBtw = $incBtw;
     }
 
 
     function calcBtw($inc){
         if($inc) {
-            $btw = this->getBtw();
-            $bedrag = this->getBedrag();
+            $btw = $this->getBtw();
+            $bedrag = $this->getBedrag();
             $money = $btw*$bedrag/121;
             return number_format($money, 2);
         } else {
-            $btw = this->getBtw();
-            $bedrag = this->getBedrag();
+            $btw = $this->getBtw();
+            $bedrag = $this->getBedrag();
             $money = $btw/100*$bedrag;
             return number_format($money, 2);
         }
     }
 }
 
-echo '
-<html>
-    <table style="width: 100%">
-        <tr>
-            <td>
-                <form method="post" action="rekenBTW.php">
-                    bedrag: <input name="bedrag" type="text" placeholder="'. $bedrag .',-" ><br>
-                    Is het aangegeven bedrag:<br>
-                    - inclusief BTW: <input type="radio" name="inc" value="true" <br><br>
-                    - exclusief BTW: <input type="radio" name="inc" value="false" ><br><br>
-                    <input type="submit" name="submit" value="submit">
-                </form>
-            <td>
-            <td>
-                btw: '. $btw .'% <br>
-                btw bedrag: €'. $btwBedrag .',- <br>
-                zonder btw: €'. $noBtw .',-<br>
-                met btw: €'. $incBtw .' <br>
-            <td>
-        </tr>
-    </table>
-</html>
-';
+$form = new form();
+
+// echo '
+// <html>
+//     <table style="width: 100%">
+//         <tr>
+//             <td>
+//                 <form method="post" action="rekenBTW.php">
+//                     bedrag: <input name="bedrag" type="text" placeholder="'. $bedrag .',-" ><br>
+//                     Is het aangegeven bedrag:<br>
+//                     - inclusief BTW: <input type="radio" name="inc" value="true" <br><br>
+//                     - exclusief BTW: <input type="radio" name="inc" value="false" ><br><br>
+//                     <input type="submit" name="submit" value="submit">
+//                 </form>
+//             <td>
+//             <td>
+//                 btw: '. $btw .'% <br>
+//                 btw bedrag: €'. $btwBedrag .',- <br>
+//                 zonder btw: €'. $noBtw .',-<br>
+//                 met btw: €'. $incBtw .' <br>
+//             <td>
+//         </tr>
+//     </table>
+// </html>
+// ';
 
