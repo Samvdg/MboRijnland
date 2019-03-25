9fcb014459e71f85ad89c714d5af78b773849b0d
Samvdg <1032961@idcollege.nl>
2019-03-25 09:11:25 +0100

    added  logboek

---
+++ Robbe/Symfony/Jochie/src/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samvd
+ * Date: 20-2-2019
+ * Time: 12:41
+ */
+
+namespace App\Entity;
+
+
+class User
+{
+
+}
---
+++ Robbe/logboek/logboek/src/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samvd
+ * Date: 18-3-2019
+ * Time: 11:35
+ */
+
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
