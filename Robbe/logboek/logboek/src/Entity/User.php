cf05e48f8ff033113226a344605e978a66cab317
Samvdg <1032961@idcollege.nl>
2019-04-04 20:25:47 +0200

    updated logboek, added some some cruds and started working on roles and their permissions

--- Robbe/logboek/logboek/src/Entity/User.php
+++ Robbe/logboek/logboek/src/Entity/User.php
@@ -26,9 +26,179 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $last_activity;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $tel_nr;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mobile_nr;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $insertion_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getLastActivity(): ?\DateTimeInterface
+    {
+        return $this->last_activity;
+    }
+
+    public function setLastActivity(\DateTimeInterface $last_activity): self
+    {
+        $this->last_activity = $last_activity;
+
+        return $this;
+    }
+
+    public function getTelNr(): ?int
+    {
+        return $this->tel_nr;
+    }
+
+    public function setTelNr(int $tel_nr): self
+    {
+        $this->tel_nr = $tel_nr;
+
+        return $this;
+    }
+
+    public function getMobileNr(): ?int
+    {
+        return $this->mobile_nr;
+    }
+
+    public function setMobileNr(int $mobile_nr): self
+    {
+        $this->mobile_nr = $mobile_nr;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getInsertionName(): ?string
+    {
+        return $this->insertion_name;
+    }
+
+    public function setInsertionName(string $insertion_name): self
+    {
+        $this->insertion_name = $insertion_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(string $zip): self
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
     }
 }
